f72bdbc2549f26cd23f288a6336b7c9b9aea9d0d
Rene Couturier <rRene333@googlemail.com>
2023-08-28 13:37:39 +0200

    strukt

--- app/items/ChartScreen.jsx
+++ app/items/ChartScreen.jsx
@@ -3,78 +3,97 @@
 import { cn } from "@/lib/utils";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
 
-const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const colorMap = {
+  "MTA": "#0088FE",
+  "SERV": "#42c72aE",
+}
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: "60",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: "61",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: "62",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: "63",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: "64",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    name: "65",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: "66",
+    vals: [{pivot:"MTA", vk: 0.4}, {pivot:"SERV", vk:0.7}],
     pk: 1,
   },
 ];
 
-const getPath = (x, y, width, height, scale) => {
+function scaleSvgPath(originalD, scalingFactor) {
+  var pattern = /[-+]?\d*\.\d+|[-+]?\d+/g;
 
+  function scaleValue(value) {
+    return (parseFloat(value) * scalingFactor).toString();
+  }
 
+  var scaledD = originalD.replace(pattern, function (match) {
+    return scaleValue(match);
+  });
 
-  return `
-  M${x} ${100}l-20,-50l20,-50l20,50l-20,50z`;
+  return scaledD;
+}
+
+const getPath = (x, y, width, height, scale, shift) => {
+  const d_raw =
+    "h-20v40c0,0,2.847,-14.721,6,-17c3.153,-2.279,9.5,-3,9.5,-3v-1c0,0,-4,-2,-4,-8c0,-6,2.5,-11,8.5,-11zm20,40c0,0,-2.847,-14.721,-6,-17c-3.153,-2.279,-9.5,-3,-9.5,-3v-1c0,0,4,-2,4,-8c0,-6,-2.5,-11,-8.5,-11h20v40zm0,22h-7.5v-18c-0.794,-1.662,-2.5,-4,-2.5,-4v60h10v-38zm-17,38l-3,-38l-3,38h6zm-13,0v-60c0,0,-1.706,2.338,-2.5,4v18l-3.75,3l-3.75,-3v38h10z";
+
+  const d = `M${x + width / 2},${y - height * shift}${scaleSvgPath(
+    d_raw,
+    scale
+  )}`;
+
+  return d;
 };
 
 const TriangleBar = (props) => {
-  console.log(props)
-  const { fill, x, y, width, height,  } = props;
+  console.log(props);
 
-  const initHeight = 100
-  const dataHeight = height
-  // const scale = dataHeight/initHeight
-  const scale = 1
+  const { fill, x, y, width, height, vals } = props;
 
-  return <path style={{transform: `scale(${scale})`}} className="origin-bottom"  d={getPath(x, y, width, height, scale)} stroke="none" fill={fill} />;
+  const initHeight = 100;
+  const dataHeight = height;
+  const scale = dataHeight / initHeight;
+  // const scale = 1
+
+  return (
+    <g>
+      {vals.map((item, index) => (
+        <path
+        d={getPath(x, y, width, height, scale, index)}
+        stroke="none"
+        fill={colorMap[item.pivot]}
+      />
+      ))}
+      
+    </g>
+  );
 };
 
 export function ChartScreen() {
@@ -82,7 +101,7 @@ export function ChartScreen() {
     <div className="container flex justify-center mt-10">
       <BarChart
         width={700}
-        height={400}
+        height={300}
         data={data}
         margin={{
           top: 20,
@@ -93,17 +112,8 @@ export function ChartScreen() {
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
-        <YAxis />
-        <Bar
-          dataKey="pk"
-          fill="#8884d8"
-          shape={<TriangleBar />}
-          label={{ position: "top" }}
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-          ))}
-        </Bar>
+        <YAxis type="number" domain={[0, 3]} tickCount={4} />
+        <Bar dataKey="pk" fill="#72718a" shape={<TriangleBar />} />
       </BarChart>
     </div>
   );
